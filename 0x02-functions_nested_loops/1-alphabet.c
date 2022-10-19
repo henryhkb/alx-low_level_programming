9f9b59df8b1dd7ac97ee36c9ea938de33cbeb1d0
root <brandohhenry@gmail.com>
2022-10-19 14:13:56 -0700

    eddited 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,21 @@
- #include "main.h"
+include "main.h"
 
 /**
 *print_alphabet - Entry point
-*Description: a function that prints an alphabet,
+*Description: a function that prints the alphabet,
 *in lowecase followed by a new line
 *Return: void
 */
 void print_alphabet(void)
 {
-	char letter = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+	char letter = 'a';
 
+	while (letter <= 'z')
+	{
 		_putchar(letter);
+		letter++;
+	}
 	_putchar('\n');
 }
+
 
